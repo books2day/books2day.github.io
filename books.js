2bba700be66057ac5869f612501ea9c0371b70fe
books2day <90879400+books2day@users.noreply.github.com>
2021-09-21 19:18:19 -0500

    Update books.js

--- books.js
+++ books.js
@@ -27,12 +27,14 @@ function load_page() {
 
         const contract = create_contract();
 
-        console.log(await contract.methods._readBook("test1").call())
         var all_books = await contract.methods._readAll().call();
 
         for (let x=0; x<all_books.length; x++) {
-            console.log(all_books[x][0], all_books[x][1], all_books[x][2])
-            create_book_layout(all_books[x][0], all_books[x][1], all_books[x][2])
+
+            if (all_books[x][0] != "") {
+                console.log(all_books[x][0], all_books[x][1], all_books[x][2])
+                create_book_layout(all_books[x][0], all_books[x][1], all_books[x][2])
+            };
         }
 
         document.querySelector("#spinner").style.display = "none";
@@ -56,8 +58,8 @@ document.querySelector("#upload-submit").addEventListener("click", async () => {
     await web3.eth.getAccounts(console.log);
 
     //Connect to the contract
-    const abi = [{"inputs":[{"internalType":"string","name":"","type":"string"}],"name":"TitleToImg","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"","type":"string"}],"name":"TitleToText","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"_title","type":"string"},{"internalType":"string","name":"_img","type":"string"},{"internalType":"string","name":"_text","type":"string"}],"name":"_createBook","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"_readAll","outputs":[{"components":[{"internalType":"string","name":"title","type":"string"},{"internalType":"string","name":"img","type":"string"},{"internalType":"string","name":"text","type":"string"}],"internalType":"struct bookStorage.Book[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"_title","type":"string"}],"name":"_readBook","outputs":[{"internalType":"string","name":"","type":"string"},{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"books","outputs":[{"internalType":"string","name":"title","type":"string"},{"internalType":"string","name":"img","type":"string"},{"internalType":"string","name":"text","type":"string"}],"stateMutability":"view","type":"function"}]
-    const address = '0x21e49B49af884674C7F2F5E59E4635A5552Df058'//0x8B4BC70F423fBB9c93324B9a0c4AA1FA1a50A2d8'//'//
+    const abi = [{"inputs":[{"internalType":"string","name":"_title","type":"string"},{"internalType":"string","name":"_img","type":"string"},{"internalType":"string","name":"_text","type":"string"}],"name":"_createBook","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"_deleteBook","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"_readAll","outputs":[{"components":[{"internalType":"string","name":"title","type":"string"},{"internalType":"string","name":"img","type":"string"},{"internalType":"string","name":"text","type":"string"}],"internalType":"struct bookStorage.Book[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"books","outputs":[{"internalType":"string","name":"title","type":"string"},{"internalType":"string","name":"img","type":"string"},{"internalType":"string","name":"text","type":"string"}],"stateMutability":"view","type":"function"}]
+    const address = '0xF7b9A7d7232d0A852eF3401edFa7728c7a83Eda4'//0x8B4BC70F423fBB9c93324B9a0c4AA1FA1a50A2d8'//'//
     const contract = new web3.eth.Contract(abi, address)
 
     const title = document.getElementById('title').value;
@@ -66,6 +68,27 @@ document.querySelector("#upload-submit").addEventListener("click", async () => {
 
 
     await contract.methods._createBook(`${title}`, `${img}`, `${hash}`).send({from: account});
+});
+
+/*////////////////////
+/// Deleting Files
+/////////////////////*/
+
+document.querySelector("#delete").addEventListener("click", async () => {
+
+    // Get the accounts
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    const account = accounts[0]
+    //Initiate web3 instance
+    let web3 = new Web3(Web3.givenProvider)
+    await web3.eth.getAccounts(console.log);
+
+    //Connect to the contract
+    const abi = [{"inputs":[{"internalType":"string","name":"_title","type":"string"},{"internalType":"string","name":"_img","type":"string"},{"internalType":"string","name":"_text","type":"string"}],"name":"_createBook","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"_deleteBook","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"_readAll","outputs":[{"components":[{"internalType":"string","name":"title","type":"string"},{"internalType":"string","name":"img","type":"string"},{"internalType":"string","name":"text","type":"string"}],"internalType":"struct bookStorage.Book[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"books","outputs":[{"internalType":"string","name":"title","type":"string"},{"internalType":"string","name":"img","type":"string"},{"internalType":"string","name":"text","type":"string"}],"stateMutability":"view","type":"function"}]
+    const address = '0xF7b9A7d7232d0A852eF3401edFa7728c7a83Eda4'//0x8B4BC70F423fBB9c93324B9a0c4AA1FA1a50A2d8'//'//
+    const contract = new web3.eth.Contract(abi, address)
+
+    await contract.methods._deleteBook().send({from: account});
 });
 
 
@@ -78,8 +101,8 @@ document.querySelector("#upload-submit").addEventListener("click", async () => {
 //////////////////////*/
 function create_contract() {
     //Connect to the contract
-    const abi = [{"inputs":[{"internalType":"string","name":"","type":"string"}],"name":"TitleToImg","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"","type":"string"}],"name":"TitleToText","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"_title","type":"string"},{"internalType":"string","name":"_img","type":"string"},{"internalType":"string","name":"_text","type":"string"}],"name":"_createBook","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"_readAll","outputs":[{"components":[{"internalType":"string","name":"title","type":"string"},{"internalType":"string","name":"img","type":"string"},{"internalType":"string","name":"text","type":"string"}],"internalType":"struct bookStorage.Book[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"_title","type":"string"}],"name":"_readBook","outputs":[{"internalType":"string","name":"","type":"string"},{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"books","outputs":[{"internalType":"string","name":"title","type":"string"},{"internalType":"string","name":"img","type":"string"},{"internalType":"string","name":"text","type":"string"}],"stateMutability":"view","type":"function"}]
-    const address = '0x21e49B49af884674C7F2F5E59E4635A5552Df058'//0x8B4BC70F423fBB9c93324B9a0c4AA1FA1a50A2d8'//'//
+    const abi = [{"inputs":[{"internalType":"string","name":"_title","type":"string"},{"internalType":"string","name":"_img","type":"string"},{"internalType":"string","name":"_text","type":"string"}],"name":"_createBook","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"_deleteBook","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"_readAll","outputs":[{"components":[{"internalType":"string","name":"title","type":"string"},{"internalType":"string","name":"img","type":"string"},{"internalType":"string","name":"text","type":"string"}],"internalType":"struct bookStorage.Book[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"books","outputs":[{"internalType":"string","name":"title","type":"string"},{"internalType":"string","name":"img","type":"string"},{"internalType":"string","name":"text","type":"string"}],"stateMutability":"view","type":"function"}]
+    const address = '0xF7b9A7d7232d0A852eF3401edFa7728c7a83Eda4'//0x8B4BC70F423fBB9c93324B9a0c4AA1FA1a50A2d8'//'//
     const contract = new web3.eth.Contract(abi, address)
     return contract;
 }
